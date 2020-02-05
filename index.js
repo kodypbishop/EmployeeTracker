ed37c3738b4948c5e4992c59a72d889ffba040f4
Kody Bishop <kodypbishop@gmail.com>
2020-02-05 16:34:56 -0700

    wrote functions and querys

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql")
+const cTable = require('console.table')
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-    user:"root",
-    password:"rootroot",
-    database:"employee_db"
+    user: "root",
+    password: "rootroot",
+    database: "employee_db"
 })
 start();
 function start() {
@@ -19,9 +20,11 @@ function start() {
                 "View all Employees by Department",
                 "View all Employees by manager",
                 "Add Employee",
+                "Add Role",
                 "Remove Employee",
                 "Update Empoyee Role",
-                "Update Empoyee Manager"
+                "Update Empoyee Manager",
+
             ]
         }
     ]).then(function (data) {
@@ -43,12 +46,18 @@ function start() {
                 addEmployee();
                 break;
 
+            case "Add Role":
+                addRole();
+                break;
+
             case "Remove Employee":
                 removeEmployee();
                 break;
+
             case "Update Empoyee Role":
                 updateEmployee();
                 break;
+
             case "Update Empoyee Manager":
                 updateManager();
                 break;
@@ -56,28 +65,234 @@ function start() {
     })
 };
 
-function viewAll(){
-    connection.query("select * from employee", function(err,res){
-        console.log(res)
+function viewAll() {
+    let q = "select a.id, a.first_name, a.last_name, concat(b.first_name,' ',b.last_name) as 'manager' ,title, salary, name from employee a left join employee b on a.manager_id = b.id  left join role on a.role_id = role.id left join department on role.department_id = department.id;"
+    connection.query(q, function (err, res) {
+        console.log("\n")
+        console.table(res)
+
+        start();
+
     })
 };
 
-function viewDepartment(){
-
+function viewDepartment() {
+    let q = "select name from department"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        console.log("\n")
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Which department would you like to see?",
+                name: "department",
+                choices: arr
+            }
+        ]).then(function (data) {
+            console.log(data.department)
+            let q = `select a.id, a.first_name, a.last_name, concat(b.first_name,' ',b.last_name) as 'manager' ,title, salary, name from employee a left join employee b on a.manager_id = b.id  left join role on a.role_id = role.id left join department on role.department_id = department.id where name = "${data.department}" `
+            connection.query(q, function (err, res) {
+                console.log("\n")
+                console.table(res)
+                start();
+            })
+        });
+    })
 };
 
-function addEmployee(){
+function viewManager() {
+    let q = "select concat(first_name, ' ', last_name) as name from employee"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        console.log("\n")
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Whose subordinates would you like to see?",
+                name: "person",
+                choices: arr
+            }
+        ]).then(function (data) {
+            data = data.person.split(" ")
+            let q = `select a.id, a.first_name, a.last_name, concat(b.first_name,' ',b.last_name) as 'manager' ,title, salary, name from employee a left join employee b on a.manager_id = b.id  left join role on a.role_id = role.id left join department on role.department_id = department.id where b.first_name ="${data[0]}" and b.last_name = "${data[1]}" `
+            connection.query(q, function (err, res) {
+                console.log("\n")
+                console.table(res)
+                start();
+            })
+        });
+    })
+}
 
+function addEmployee() {
+    let employee;
+    let q = "select id, concat(first_name, ' ', last_name) as name from employee"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        arr.push("Self Supervised")
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "What is the employee's first name?",
+                name: "first",
+            },
+            {
+                type: "input",
+                message: "What is the employee's last name?",
+                name: "last",
+            },
+            {
+                type: "list",
+                message: "Who is the employee's manager?",
+                name: "manager",
+                choices: arr
+            }
+        ]).then(function (data) {
+            if (data.manager == "Self Supervised") {
+                data.manager_id = null
+            } else {
+                data.manager_id = res.filter(each => each.name == data.manager)[0].id
+            }
+            employee = data
+            let q = "select id, title from role"
+            connection.query(q, function (err, res) {
+                let arr = res.map(each => each.title)
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        message: "What is there role?",
+                        name: "role",
+                        choices: arr
+                    }
+                ]).then(function (data) {
+                    employee.role_id = res.filter(each => each.title == data.role)[0].id
+                    q = `insert into employee (first_name, last_name, role_id, manager_id)
+                    values ("${employee.first}","${employee.last}",${employee.role_id},${employee.manager_id})`;
+                    connection.query(q, function (err, res) {
+                        start();
+                    })
+                })
+            });
+        })
+    });
 };
 
-function removeEmployee(){
-
+function addRole() {
+    let role;
+    let q = "select id, name from department"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "In what department is the new role?",
+                name: "name",
+                choices: arr
+            },
+            {
+                type: "input",
+                message: "What is the role's title?",
+                name: "title",
+            },
+            {
+                type: "input",
+                message: "What is the roles salary?",
+                name: "salary",
+            },
+        ]).then(function (data) {
+            data.department_id = res.filter(each => each.name == data.name)[0].id
+            q = `insert into role (title, salary, department_id)
+                    values ("${data.title}","${data.salary}",${data.department_id})`;
+            connection.query(q, function (err, res) {
+                start();
+            })
+        })
+    });
 };
 
-function updateEmployee(){
-
+function removeEmployee() {
+    let q = "select id, concat(first_name, ' ', last_name) as name from employee"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Whose role would you like to change?",
+                name: "name",
+                choices: arr
+            }
+        ]).then(function (data) {
+            data = res.filter(each => each.name == data.name)[0].id;
+            q = `delete from employee where id = "${data}"`
+            connection.query(q, function (err, res) {
+                start();
+            })
+        })
+    })
 };
 
-function updateManager(){
+function updateEmployee() {
+    let change;
+    let q = "select id, concat(first_name, ' ', last_name) as name from employee"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Whose role would you like to change?",
+                name: "name",
+                choices: arr
+            }
+        ]).then(function (data) {
+            data.id = res.filter(each => each.name == data.name)[0].id;
+            change = data;
+            q = `select id, title from role`
+            connection.query(q, function (err, res) {
+                let arr = res.map(each => each.title)
+                inquirer.prompt([
+                    {
+                        type: "list",
+                        message: "What role would you like them to have?",
+                        name: "role",
+                        choices: arr
+                    }
+                ]).then(function (data) {
+                    change.role_id = res.filter(each => each.title == data.role)[0].id;
+                    q = `update employee set role_id = "${change.role_id}" where id = "${change.id}" `;
+                    connection.query(q, function (err, res) {
+                        start();
+                    });
+                });
+            });
+        });
+    });
+};
 
+function updateManager() {
+
+    let q = "select id, concat(first_name, ' ', last_name) as name from employee"
+    connection.query(q, function (err, res) {
+        let arr = res.map(each => each.name)
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Whose manager would you like to change?",
+                name: "name",
+                choices: arr
+            },
+            {
+                type: "list",
+                message: "Whose would you like to be their manager?",
+                name: "manager",
+                choices: arr
+            }
+        ]).then(function (data) {
+            data.id = res.filter(each => each.name == data.name)[0].id;
+            data.manager_id = res.filter(each => each.name == data.manager)[0].id;
+            q = `update employee set manager_id = "${data.manager_id}" where id = "${data.id}" `;
+            connection.query(q, function (err, res) {
+                start();
+            })
+        })
+    })
 };
--- schema.sql
+++ schema.sql
@@ -7,7 +7,8 @@ create table employee(
     id int(10) primary key auto_increment not null,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
-    role_id int(10) not null
+    role_id int(10) not null,
+    manager_id int(10)
 );
 
 create table role(
@@ -17,7 +18,16 @@ create table role(
     department_id int(10) not null
 );
 
-create table dapartment(
+create table department(
     id int(10) primary key auto_increment not null,
     name varchar(30) not null
 );
+
+insert into employee (first_name, last_name, role_id, manager_id)
+values ("Kody","Bishop",1,3),("Paul","John",2,3),("Dave","Johnson",3,null),("Bill","Anderson",4,5),("Jane","Smith",5,null),("Spike","Lee",6,7),("Karen","Bing",7,null);
+
+insert into role (title, salary, department_id)
+values ("Junior Dev","55k",1),("Senior Dev","75k",1),("Tech Supervisor","120k",1),("Designer","50k",2),("Head Designer","80k",2),("Salesperson","45k",3),("Employee Support","70k",4);
+
+insert into department (name)
+values ("Tech"),("Design"),("Sales"),("HR");
